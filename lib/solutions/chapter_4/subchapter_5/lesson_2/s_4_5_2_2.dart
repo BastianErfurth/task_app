e03c52cd1ca1783b7403e34e1dcb456b2d3c23f2
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-04-02 11:32:35 +0200

    solved 4522

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,27 @@ class S4522 extends StatelessWidget {
   const S4522({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyStatelessWidget();
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Hello Bastian"),
+            Center(
+              child: ElevatedButton(onPressed: null, child: Text("Klick mich")),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
